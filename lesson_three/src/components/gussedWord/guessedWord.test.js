beb92712dc662a0b19866fdf163a22ca78d51b27
pratyakash <pratyakash13@gmail.com>
2021-09-23 15:55:14 +0530

    Guess word Test (Descirbe)

--- lesson_three/src/components/gussedWord/guessedWord.test.js
+++ lesson_three/src/components/gussedWord/guessedWord.test.js
@@ -22,3 +22,25 @@ const setup = (props = {}) => {
 test('does not throw warning with expected props', () => {
   checkProps(GuessedWordComponent, defaultProps);
 });
+
+describe('if no words are guessed', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessedword');
+
+    expect(component.length).toBe(1);
+  });
+
+  test('renders instruction to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe('if words are guessed', () => {});
--- lesson_three/src/components/gussedWord/index.js
+++ lesson_three/src/components/gussedWord/index.js
@@ -1,20 +1,26 @@
 import PropTypes from 'prop-types';
 
-const GuessedWord = ({}) => {
-  return (
-    <div>
-      <div></div>
-    </div>
-  );
+const GuessedWord = ({ guessedWords }) => {
+  let contents;
+
+  if (guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">
+        Try to guess the secrect word !
+      </span>
+    );
+  }
+
+  return <div data-test="component-guessedword">{contents}</div>;
 };
 
-GuessedWord.propType = {
+GuessedWord.propTypes = {
   guessedWords: PropTypes.arrayOf(
     PropTypes.shape({
       guessedWord: PropTypes.string.isRequired,
       letterMatchCount: PropTypes.number.isRequired,
     })
-  ),
+  ).isRequired,
 };
 
 export default GuessedWord;
