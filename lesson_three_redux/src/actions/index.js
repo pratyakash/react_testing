a692b3c5b2228ffa9fb17db5632a83dc21b17b21
pratyakash <pratyakash13@gmail.com>
2021-09-24 14:04:55 +0530

    Redux Setup

--- lesson_three_redux/src/actions/index.js
+++ lesson_three_redux/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const getSecretWord = () => {
-  return axios.get("http://localhost: 3030").then((response) => response.data);
+  return axios.get("http://localhost:3030").then((response) => response.data);
 };
---
+++ lesson_three_redux/src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import successReducer from './successReducer';
+
+export default combineReducers({
+    successReducer
+})
