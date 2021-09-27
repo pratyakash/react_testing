92f82e450cfb79baeb9cab0fcafc8ce80afeee9b
pratyakash <pratyakash13@gmail.com>
2021-09-27 10:49:06 +0530

    Redux Testing Init

--- lesson_three_redux/src/actions/index.js
+++ lesson_three_redux/src/actions/index.js
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const getSecretWord = () => {
   return axios.get("http://localhost:3030").then((response) => response.data);
 };
+
+export const actionTypes = {
+  CORRECT_GUESS: 'correct_guess'
+};
+
+export const correctGuess = () => {
+  return {
+    type: actionTypes.CORRECT_GUESS,
+  };
+};
